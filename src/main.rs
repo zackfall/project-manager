30a23cd5ea3f6758142ac3d205bdc016ca901134
zackfall <zackfall@outlook.com>
2022-11-05 21:56:57 -0500

    doc: add missing_doc warning for the project

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+#![warn(missing_docs)]
+
+//! A CLI task manager writen with tui-rs, for personal use.
+
 fn main() {
-    println!("Hello, world!");
+    let h = String::from("Hola mundo");
+    println!("{}!", h);
 }
 
 #[cfg(test)]
