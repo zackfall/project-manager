bfce57a5a1d665c1c730f7976dfb35ab4102a1da
zackfall <zackfall@outlook.com>
2022-11-06 14:10:13 -0500

    feat: create and connect the database, refs #1

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,29 @@
 //! A CLI task manager writen with tui-rs, for personal use.
 
 use futures::executor::block_on;
-use sea_orm::{Database, DbErr};
+use sea_orm::{ConnectionTrait, Database, DbErr, Statement};
 
 const DATABASE_URI: &str = "postgres://postgres@localhost:5432";
 const DB_NAME: &str = "task_manager";
 
 async fn run() -> Result<(), DbErr> {
     let db = Database::connect(DATABASE_URI).await?;
+
+    let db = {
+        db.execute(Statement::from_string(
+            db.get_database_backend(),
+            format!("DROP DATABASE IF EXISTS \"{}\";", DB_NAME),
+        ))
+        .await?;
+        db.execute(Statement::from_string(
+            db.get_database_backend(),
+            format!("CREATE DATABASE \"{}\";", DB_NAME),
+        ))
+        .await?;
+
+        let url = format!("{}/{}", DATABASE_URI, DB_NAME);
+        Database::connect(&url).await?
+    };
 
     Ok(())
 }
