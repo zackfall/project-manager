982b3d621b7c7cab56bb078edc8bd680f10a6a48
zackfall <zackfall@outlook.com>
2022-11-30 13:01:40 -0500

    feat: using the GithubAPI, instead of a Database, close 8

--- src/main.rs
+++ src/main.rs
@@ -2,83 +2,10 @@
 
 //! A CLI task manager writen with tui-rs, for personal use.
 
-use futures::executor::block_on;
-use migration::{Migrator, MigratorTrait};
-use sea_orm::{ConnectionTrait, Database, DatabaseConnection, DbBackend, DbErr, Statement};
-
-/// This struct manage the connection with the database
-/// it also run queries and actions to interact with the database
-/// It will be moved to another file in the future.
-struct DatabaseConn<'a> {
-    db_url: &'a str,
-    /// Is an Optional Value because, there is the case (the actual case) where you
-    /// want to use SQLite, so you don't need this.
-    db_name: Option<&'a str>,
-}
-
-impl<'a> DatabaseConn<'a> {
-    /// Creates a new instance of DatabaseConn
-    pub fn new(db_url: &'a str, db_name: Option<&'a str>) -> Self {
-        Self { db_url, db_name }
-    }
-
-    /// Connects to the database.
-    ///
-    /// We use a match statement, in the case we want to use another dabatase
-    /// backend in the future.
-    pub async fn connect(self) -> Result<DatabaseConnection, DbErr> {
-        let db = Database::connect(self.db_url).await?;
-        let db = match db.get_database_backend() {
-            DbBackend::MySql => {
-                db.execute(Statement::from_string(
-                    db.get_database_backend(),
-                    format!("CREATE DATABASE IF NOT EXISTS `{}`;", self.db_name.unwrap()),
-                ))
-                .await?;
-
-                let url = format!("{}/{}", self.db_url, self.db_name.unwrap());
-                Database::connect(&url).await?
-            }
-            DbBackend::Postgres => {
-                db.execute(Statement::from_string(
-                    db.get_database_backend(),
-                    format!("DROP DATABASE IF EXISTS \"{}\";", self.db_name.unwrap()),
-                ))
-                .await?;
-                db.execute(Statement::from_string(
-                    db.get_database_backend(),
-                    format!("CREATE DATABASE \"{}\";", self.db_name.unwrap()),
-                ))
-                .await?;
-
-                let url = format!("{}/{}", self.db_url, self.db_name.unwrap());
-                Database::connect(&url).await?
-            }
-            DbBackend::Sqlite => db,
-        };
-        Migrator::up(&db, None).await?;
-
-        Ok(db)
-    }
-}
+use octocrab::{models::Repository, Result};
+mod actions;
+use actions::get_actions;
 
 fn main() {
-    let db_url = std::env::var("DATABASE_URL").unwrap_or_else(|_| "sqlite::memory:".to_owned());
-    let db = DatabaseConn::new(db_url.as_str(), None);
-    if let Err(err) = block_on(db.connect()) {
-        panic!("{}", err);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn test_connection_to_database() {
-        let db_url = "sqlite::memory:";
-        let db = DatabaseConn::new(db_url, None);
-        let res: Result<DatabaseConnection, DbErr> = db.connect().await;
-        assert!(res.is_ok());
-    }
+    todo!();
 }
