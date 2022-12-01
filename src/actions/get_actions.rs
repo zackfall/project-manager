858660938d43342e8872ba46799e028a10718390
zackfall <zackfall@outlook.com>
2022-12-01 16:54:09 -0500

    feat: created actions for the tests, close #9

--- src/actions/get_actions.rs
+++ src/actions/get_actions.rs
@@ -39,6 +39,53 @@ pub async fn get_issues_by_assignee(
     Ok(assignee)
 }
 
+pub async fn get_issues_by_creator(owner: &str, repo: &str, creator: &str) -> Result<Page<Issue>> {
+    let creator = build_octo()
+        .issues(owner, repo)
+        .list()
+        .creator(creator.to_owned())
+        .state(params::State::All)
+        .per_page(15)
+        .page(1u8)
+        .send()
+        .await?;
+    Ok(creator)
+}
+
+pub async fn get_issues_by_state(
+    owner: &str,
+    repo: &str,
+    state: params::State,
+) -> Result<Page<Issue>> {
+    let creator = build_octo()
+        .issues(owner, repo)
+        .list()
+        .state(state)
+        .state(params::State::All)
+        .per_page(15)
+        .page(1u8)
+        .send()
+        .await?;
+    Ok(creator)
+}
+
+pub async fn get_issues_by_labels(
+    owner: &str,
+    repo: &str,
+    labels: &[String],
+) -> Result<Page<Issue>> {
+    let creator = build_octo()
+        .issues(owner, repo)
+        .list()
+        .labels(labels)
+        .state(params::State::All)
+        .per_page(15)
+        .page(1u8)
+        .send()
+        .await?;
+    Ok(creator)
+}
+
 pub async fn get_repo(owner: &str, repo: &str) -> Result<Repository> {
     let repo = build_octo().repos(owner, repo).get().await?;
     Ok(repo)
