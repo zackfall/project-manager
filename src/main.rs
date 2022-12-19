fb95cb0d2f8f9f8a2fcbe7f96c5bc75cc560d32f
zackfall <zackfall@outlook.com>
2022-12-18 23:49:15 -0500

    fix: delete all the TUI references, close #12

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,6 @@
 
 //! A CLI task manager writen with tui-rs, for personal use.
 
-use std::io;
-use tui::backend::CrosstermBackend;
-
-fn main() -> Result<(), std::io::Error> {
-    let stdout = io::stdout();
-    let backend = CrosstermBackend::new(stdout);
-    let app = ui::run_ui(backend)?;
-    Ok(app)
+fn main() {
+    println!("Working!")
 }
--- tests/ui_test.rs
+++
@@ -1,8 +0,0 @@
-use std::io;
-use tui::backend::TestBackend;
-
-#[test]
-fn ui_initialization_test() {
-    let backend = TestBackend::new(100, 100);
-    assert!(ui::run_ui(backend).is_ok());
-}
